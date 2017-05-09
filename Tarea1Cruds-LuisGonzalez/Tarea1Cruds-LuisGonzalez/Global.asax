baad676bc2f6ae757cde9aefcec4d91ee06eb069
LuisGonzalez <luis.rigoma@gmail.com>
2017-05-09 15:46:39 -0600

    Se agrego el proyecto

---
+++ Tarea1Cruds-LuisGonzalez/Tarea1Cruds-LuisGonzalez/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tarea1Cruds_LuisGonzalez.MvcApplication" Language="C#" %>
---
+++ Tarea1Cruds-LuisGonzalez/Tarea1Cruds-LuisGonzalez/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tarea1Cruds_LuisGonzalez.Startup))]
+namespace Tarea1Cruds_LuisGonzalez
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
