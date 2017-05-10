f230505288c969c2179bb6437aa9147e6a60d866
LuisGonzalez <luis.rigoma@gmail.com>
2017-05-09 18:29:46 -0600

    Se agrego el entity y crud de departamento para test

---
+++ Tarea1Cruds-LuisGonzalez/Tarea1Cruds-LuisGonzalez/Controllers/DepartamentoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tarea1Cruds_LuisGonzalez.DB;
+
+namespace Tarea1Cruds_LuisGonzalez.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        // GET: Departamento
+        public ActionResult Index()
+        {
+
+            using (var context = new ModelDBContainer())
+            {
+                var list = context.DepartamentoSet.ToList();
+                return View(list);
+            }
+                
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Departamento model)
+        {
+            using (var context = new ModelDBContainer())
+            {
+                context.DepartamentoSet.Add(model);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+
+
+
+
+    }
+}
